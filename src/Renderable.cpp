84e28edf992530e24e1b33cb50f18fd4f4b5d4f3
Yadi <dylanyasen@gmail.com>
2020-10-12 23:56:20 -0700

    renderable

---
+++ src/Renderable.cpp
@@ -0,0 +1,17 @@
+#include "Renderable.h"
+
+#include <vector>
+
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+
+Renderable::Renderable()
+{
+}
+
+Renderable::Renderable(const std::vector<Vertex> &v, const std::vector<unsigned int> &i)
+    : m_vertices(v), m_indices(i)
+{
+}
---
+++ src/Renderable.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Transform.h"
+#include <vector>
+#include <string>
+
+class Texture;
+class VertexArray;
+class VertexBuffer;
+class IndexBuffer;
+class Shader;
+
+class Renderer;
+class Camera;
+
+struct Vertex
+{
+    vec3 position;
+    vec3 normal;
+    vec3 tangent;
+    vec3 bitangent;
+    vec2 texCoords;
+};
+
+class Renderable
+{
+public:
+    Renderable();
+    Renderable(const std::vector<Vertex> &v, const std::vector<unsigned int> &i);
+    virtual void Render(const Camera* camera, const Renderer *renderer) = 0;
+    virtual void Destroy() = 0;
+
+    void SetShader(Shader *shader) { m_shader = shader; };
+
+public:
+    Transform m_transform;
+
+protected:
+    std::vector<Vertex> m_vertices;
+    std::vector<unsigned int> m_indices;
+
+    std::vector<Texture *> m_textures;
+
+    VertexArray *m_vao;
+    VertexBuffer *m_vbo;
+    IndexBuffer *m_ibo;
+    Shader *m_shader;
+
+    std::string m_name;
+};

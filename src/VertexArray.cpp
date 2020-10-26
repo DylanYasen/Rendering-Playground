92753eb340976b7106d30518a3b676efff4e6eba
Yadi <dylanyasen@gmail.com>
2020-10-25 21:39:38 -0700

    for int types, use glVertexAttribIPointer

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -13,20 +13,28 @@ VertexArray::~VertexArray()
 	GLCall(glDeleteVertexArrays(1, &rendererID));
 }
 
-void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout)
+void VertexArray::AddBuffer(const VertexBuffer &vb, const VertexBufferLayout &layout)
 {
 	Bind();
 	vb.Bind();
 
-	const auto& elements = layout.GetElements();
-    const unsigned int stride = layout.GetStride();
-    unsigned int offset = 0;
-    
+	const auto &elements = layout.GetElements();
+	const unsigned int stride = layout.GetStride();
+	unsigned int offset = 0;
+
 	for (unsigned int i = 0; i < elements.size(); i++)
 	{
-		const auto& element = elements[i];
+		const auto &element = elements[i];
 		GLCall(glEnableVertexAttribArray(i));
-		GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, stride, (const void*)offset));
+		if (element.type == GL_UNSIGNED_INT ||
+			element.type == GL_INT)
+		{
+			GLCall(glVertexAttribIPointer(i, element.count, element.type, stride, (const void *)offset));
+		}
+		else
+		{
+			GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, stride, (const void *)offset));
+		}
 
 		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
 	}
--- src/VertexArray.h
+++ src/VertexArray.h
@@ -7,12 +7,12 @@ class VertexArray
 {
 private:
 	unsigned int rendererID;
+
 public:
 	VertexArray();
 	~VertexArray();
 
-
-	void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+	void AddBuffer(const VertexBuffer &vb, const VertexBufferLayout &layout);
 
 	void Bind() const;
 	void Unbind() const;

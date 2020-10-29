fe5d8e93a39e0d467527cbc5c378deeb020c04b3
Yadi <dylanyasen@gmail.com>
2020-10-28 22:16:20 -0700

    light view matrix

--- src/Light.cpp
+++ src/Light.cpp
@@ -1,25 +1,43 @@
 #include "Light.h"
 
 #include "imgui.h"
+#include "Math.h"
+
+#include <glm/gtc/matrix_transform.hpp>
 
 Light::Light()
     : ambient(vec3(0.1)),
       diffuse(vec3(1)),
-      specular(vec3(0.2))
+      specular(vec3(0.2)),
+      width(80), height(80),
+      near(1.0), far(1000),
+      transform(vec3(0, 200, 100))
 {
+    projectionMatrix = glm::ortho(-width, width, height, -height, near, far);
+    viewMatrix = glm::lookAt(transform.position, vec3(0, 0, 0), vec3(0, 0, 1));
+    lightSpaceMatrix = projectionMatrix * viewMatrix;
 }
 
 Light::~Light()
 {
 }
 
-void Light::DrawDebugMenu()
+bool Light::DrawDebugMenu()
 {
     if (ImGui::CollapsingHeader("light"))
     {
-        ImGui::InputFloat3("position", &transform.position.x);
-        ImGui::ColorEdit3("ambient", &ambient.x);
-        ImGui::ColorEdit3("diffuse", &diffuse.x);
-        ImGui::ColorEdit3("specular", &specular.x);
+        bool changed = ImGui::InputFloat3("position", &transform.position.x);
+        changed |= ImGui::ColorEdit3("ambient", &ambient.x);
+        changed |= ImGui::ColorEdit3("diffuse", &diffuse.x);
+        changed |= ImGui::ColorEdit3("specular", &specular.x);
+
+        if (changed)
+        {
+            viewMatrix = glm::lookAt(transform.position, vec3(0, 50, 0), vec3(0, 0, 1));
+            lightSpaceMatrix = projectionMatrix * viewMatrix;
+        }
+        return changed;
     }
+
+    return false;
 }
--- src/Light.h
+++ src/Light.h
@@ -8,10 +8,17 @@ public:
     Light();
     ~Light();
 
-    void DrawDebugMenu();
+    bool DrawDebugMenu();
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
     Transform transform;
+
+    float near, far;
+    float width, height;
+
+    mat4 projectionMatrix;
+    mat4 viewMatrix;
+    mat4 lightSpaceMatrix;
 };

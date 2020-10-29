617053a086d6479c6e0ac78d630ed4ffa96846b7
Yadi <dylanyasen@gmail.com>
2020-10-28 22:28:01 -0700

    added shadow pass & ground

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -3,11 +3,54 @@
 
 #include "Camera.h"
 #include "Light.h"
+#include "Shader.h"
+
+// shadow map temp
+#include "ShadowMap.h"
+#include "gl.h"
+#include "FullscreenQuad.h"
+#include "Texture.h"
+#include "Quad.h"
+
+// debug
 #include "imgui.h"
+
+const int WIDTH = 1024;
+const int HEIGHT = 768;
+
+FullscreenQuad *quad = nullptr;
+
+Quad *ground = nullptr;
 
 Scene::Scene(std::string name)
     : m_name(name)
 {
+    shadowMap = new ShadowMap();
+
+    {
+        diffuseShader = new Shader("resources/shaders/material.shader");
+        shadowMapShader = new Shader("resources/shaders/shadowMap.shader");
+        renderDepthShader = new Shader("resources/shaders/renderDepth.shader");
+        defaultShader = new Shader("resources/shaders/default.shader");
+    }
+
+    quad = new FullscreenQuad();
+
+    ground = new Quad();
+    ground->m_transform.scale = vec3(100, 100, 1);
+    ground->m_transform.rotation = glm::angleAxis(glm::radians(90.0f), vec3(1, 0, 0));
+
+    {
+        diffuseShader->Bind();
+        diffuseShader->SetUniform1i("shadowMap", shadowMap->depthMap->rendererID);
+        diffuseShader->Unbind();
+    }
+
+    {
+        defaultShader->Bind();
+        defaultShader->SetUniform1i("shadowMap", shadowMap->depthMap->rendererID);
+        defaultShader->Unbind();
+    }
 }
 
 Scene::~Scene()
@@ -16,18 +59,112 @@ Scene::~Scene()
     {
         r->Destroy();
     }
+
+    delete diffuseShader;
+    delete shadowMapShader;
+    delete renderDepthShader;
+
+    delete shadowMap;
+    delete quad;
 }
 
-void Scene::Render(Renderer *renderer) const
+void Scene::SetCamera(Camera *camera)
 {
-    for (auto &r : m_renderables)
+    m_camera = camera;
+
+    const mat4 &vp = camera->projMatrix * camera->viewMatrix;
+
     {
-        r->PreRender(this, renderer);
-        r->Render(this, renderer);
+        diffuseShader->Bind();
+        diffuseShader->SetUniform3f("viewPos", camera->eyePos);
+        diffuseShader->SetUniformMat4f("u_vp", vp);
+        diffuseShader->Unbind();
     }
 
+    {
+        defaultShader->Bind();
+        // defaultShader->SetUniform3f("viewPos", camera->eyePos);
+        defaultShader->SetUniformMat4f("u_vp", vp);
+        defaultShader->Unbind();
+    }
+}
+
+void Scene::SetLight(Light *light)
+{
+    m_light = light;
+
+    {
+        diffuseShader->Bind();
+        diffuseShader->SetUniform3f("light.ambient", light->ambient);
+        diffuseShader->SetUniform3f("light.diffuse", light->diffuse);
+        diffuseShader->SetUniform3f("light.specular", light->specular);
+        diffuseShader->SetUniform3f("lightPos", light->transform.position);
+        diffuseShader->SetUniformMat4f("lightSpaceMatrix", light->lightSpaceMatrix);
+        diffuseShader->Unbind();
+    }
+
+    {
+        shadowMapShader->Bind();
+        shadowMapShader->SetUniformMat4f("lightSpaceMatrix", light->lightSpaceMatrix);
+        shadowMapShader->Unbind();
+    }
+
+    {
+        defaultShader->Bind();
+        defaultShader->SetUniformMat4f("lightSpaceMatrix", light->lightSpaceMatrix);
+        defaultShader->Unbind();
+    }
+}
+
+void Scene::Render(Renderer *renderer)
+{
+    static const auto renderScene = [&](Shader *shader) {
+        for (auto &r : m_renderables)
+        {
+            shader->Bind();
+            r->PreRender(this, renderer, shader);
+            r->Render(this, renderer, shader);
+            shader->Unbind();
+        };
+    };
+
+    // render shadow map
+    shadowMap->Bind(m_light->lightSpaceMatrix);
+    renderScene(shadowMapShader);
+    {
+        shadowMapShader->Bind();
+        ground->PreRender(this, renderer, shadowMapShader);
+        ground->Render(this, renderer, shadowMapShader);
+        shadowMapShader->Unbind();
+    }
+    shadowMap->UnBind();
+
+    // reder scene normal
+    {
+        GLCall(glViewport(0, 0, WIDTH, HEIGHT));
+        GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+
+        shadowMap->depthMap->Bind();
+
+        renderScene(diffuseShader);
+
+        // ground
+        {
+            defaultShader->Bind();
+            ground->PreRender(this, renderer, defaultShader);
+            ground->Render(this, renderer, defaultShader);
+            defaultShader->Unbind();
+        }
+
+        shadowMap->depthMap->Unbind();
+    }
+
+    shadowMap->DrawDebugMenu();
+
     ImGui::Begin("scene", 0, ImGuiWindowFlags_AlwaysAutoResize);
-    m_camera->DrawDebugMenu();
-    m_light->DrawDebugMenu();
+    if (m_camera->DrawDebugMenu())
+        SetCamera(m_camera);
+    if (m_light->DrawDebugMenu())
+        SetLight(m_light);
     ImGui::End();
 }
--- src/Scene.h
+++ src/Scene.h
@@ -5,6 +5,7 @@ class Renderable;
 class Camera;
 class Light;
 class Renderer;
+class ShadowMap;
 
 class Scene
 {
@@ -13,11 +14,13 @@ public:
     Scene(std::string name = "untitled");
     ~Scene();
 
-    void Render(Renderer* renderer) const;
+    void Render(Renderer* renderer);
 
     void AddRenderable(Renderable *r) { m_renderables.push_back(r); };
-    void SetCamera(Camera *camera) { m_camera = camera; };
-    void SetLight(Light *light) { m_light = light; }
+    void SetCamera(Camera *camera);
+    void SetLight(Light *light);
+
+    void UseDiffuseShader();
 
     Camera *GetCamera() const { return m_camera; }
     Light *GetLight() const { return m_light; }
@@ -27,4 +30,11 @@ private:
     Camera *m_camera;
     Light *m_light;
     std::string m_name;
+
+    ShadowMap* shadowMap;
+
+    class Shader* diffuseShader;
+    class Shader* shadowMapShader;
+    class Shader* renderDepthShader;
+    class Shader* defaultShader;
 };
